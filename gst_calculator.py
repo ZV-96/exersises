ea32734dd1966e8039986b474a04079bbe626e3b
ZV-96 <vanrooyenz@middleton.school.nz>
2022-02-09 10:30:26 +1300

    backup

---
+++ gst_calculator.py
@@ -0,0 +1,8 @@
+GST = 0.15
+item = float(input("enter item price: "))
+item_GST = item * GST
+total_cost = item + item_GST
+print("total cost: $", total_cost)
+print("-----------------------------")
+
+numgig = input("
