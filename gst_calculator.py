69bc24f7016abddd36c74cfc1723d52200b674ae
ZV-96 <vanrooyenz@middleton.school.nz>
2022-02-10 09:06:59 +1300

    UPDATE

--- gst_calculator.py
+++ gst_calculator.py
@@ -5,13 +5,3 @@ total_cost = item + item_GST
 print("total cost: $", total_cost)
 print("-----------------------------")
 
-meters = int(input("enter an number of meters for converstion "))
-print(meters, "meters is:")
-print(meters * 1000, "milimeters")
-print(meters * 100, "centimeters")
-print(meters / 1000, "kilometeres")
-print(meters * 39.3701, "inchs")
-print(meters * 3.28, "feet")
-print(meters * 0.0006, "miles")
-print(meters * 1.093613888889, "yards")
-
