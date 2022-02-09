3df579ce408271d10b1a87f64aab303be1b06555
ZV-96 <vanrooyenz@middleton.school.nz>
2022-02-10 09:06:31 +1300

    backup

--- Meters - exercise.py
+++ Meters - exercise.py
@@ -1,11 +1,4 @@
-GST = 0.15
-item = float(input("enter item price: "))
-item_GST = item * GST
-total_cost = item + item_GST
-print("total cost: $", total_cost)
-print("-----------------------------")
-
-meters = int(input("enter an number of meters for converstion "))
+meters = int(input("Enter an number of meters for converstion "))
 print(meters, "meters is:")
 print(meters * 1000, "milimeters")
 print(meters * 100, "centimeters")
@@ -14,4 +7,4 @@ print(meters * 39.3701, "inchs")
 print(meters * 3.28, "feet")
 print(meters * 0.0006, "miles")
 print(meters * 1.093613888889, "yards")
-
+print("------------------------------------------")
