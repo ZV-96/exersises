72d893d151b2c41aa79e2ca374441efc7064c1fc
ZV-96 <vanrooyenz@middleton.school.nz>
2022-02-14 12:32:17 +1300

    backups

---
+++ Swiming pool tez.py
@@ -0,0 +1,7 @@
+age = int(input("oi mate wats ur age fam: "))
+if age >= 18:
+    print("u r paying more fam soz baz")
+elif age >= 13:
+    print("sup little man u pay a little less")
+else:
+    print("was up big man u the boss you go in for free")
