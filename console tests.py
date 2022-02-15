bcbb10f3e0afec431f3505a6673c1f4f7d2de523
ZV-96 <vanrooyenz@middleton.school.nz>
2022-02-16 10:54:14 +1300

    backup

---
+++ console tests.py
@@ -0,0 +1,13 @@
+number = 0
+while number <= 100:
+    print(number)
+    number += 1
+    if number % 5 == 0 and number % 3 == 0:
+    print("fizzbuzz")
+    elif number % 3 == 0:
+    print("fizz")
+    elif number % 5 == 0:
+    print("buzz")
+
+
+
