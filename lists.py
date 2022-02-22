154126b9bb68763923a10368c885ff4c9810ccdc
ZV-96 <vanrooyenz@middleton.school.nz>
2022-02-23 08:59:01 +1300

    back up

---
+++ lists.py
@@ -0,0 +1,18 @@
+numbers = [10,20,30,40]
+fruit = ["banana", "apple", "orange"]
+
+for thing in numbers:
+    print(thing)
+print("----------------------------------------")
+print(len(numbers))
+print("----------------------------------------")
+for other_thing in fruit:
+    print(other_thing)
+print("----------------------------------------")
+print(len(fruit))
+print("----------------------------------------")
+
+
+print(numbers[1])
+print(fruit[-2])
+print(fruit[-1])
